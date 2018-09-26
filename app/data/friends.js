e88654ae6770998415cec31a0f411966e794f135
ajz003 <ajz003@ucla.edu>
2018-09-26 15:09:35 -0700

    add modal with name and picture of match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends_db = [
 {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Ahmed_I_by_John_Young_%28cropped%29.jpg/330px-Ahmed_I_by_John_Young_%28cropped%29.jpg",
     "scores":[
         5,
         1,
